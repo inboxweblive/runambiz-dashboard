a2fd06a53bdca9e85fe8f2fab48fea1b628254b6
inboxweblive <inboxweblive@gmail.com>
2026-09-02 09:39:58 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from "path";
 
 const root = import.meta.dirname;
 
-const pages = ["index", "admin", "claim"];
+const pages = ["index", "admin", "claim" "store"];
 
 export default defineConfig({
   plugins: [react()],
